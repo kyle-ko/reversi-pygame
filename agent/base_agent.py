bc4fffdb07a4f9ab1392514be13ec460ac6883fa
kyle-ko <74885544+kyle-ko@users.noreply.github.com>
2020-12-06 21:08:48 +0800

    Update base_agent.py

--- agent/base_agent.py
+++ agent/base_agent.py
@@ -69,6 +69,3 @@ if __name__ == "__main__":
     agent = RandomAgent()
     print(agent.step(None, None))
 
-class MyAgent(BaseAgent):
-    def step(self, obs, reward):
-        return (self.col_offset + random.randint(0, self.cols_n-1) * self.block_len, self.row_offset + random.randint(0, self.rows_n-1) * self.block_len), pygame.USEREVENT
