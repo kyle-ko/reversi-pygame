3afa003ac77c53ccd54b2aa2b787d01afe973d47
kyle-ko <74885544+kyle-ko@users.noreply.github.com>
2020-12-06 21:19:56 +0800

    Update kyle-ko.py

--- agent/kyle-ko.py
+++ agent/kyle-ko.py
@@ -1,3 +1,8 @@
+import random
+import pygame
+import sys
+from pygame.constants import MOUSEBUTTONDOWN, MOUSEMOTION
+from base_agent.py import BaseAgent
 class MyAgent(BaseAgent):
     def step(self, obs, reward):
         return (self.col_offset + random.randint(0, self.cols_n-1) * self.block_len, self.row_offset + random.randint(0, self.rows_n-1) * self.block_len), pygame.USEREVENT
