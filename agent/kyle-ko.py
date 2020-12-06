61966aee6a83d2a648819afb7cf2ddee9490fe79
kyle-ko <74885544+kyle-ko@users.noreply.github.com>
2020-12-06 21:32:24 +0800

    Update kyle-ko.py

--- agent/kyle-ko.py
+++ agent/kyle-ko.py
@@ -2,7 +2,46 @@ import random
 import pygame
 import sys
 from pygame.constants import MOUSEBUTTONDOWN, MOUSEMOTION
-from base_agent.py import BaseAgent
+
+class BaseAgent():
+    def __init__(self, color = "black", rows_n = 8, cols_n = 8, width = 600, height = 600):
+        self.color = color
+        self.rows_n = rows_n
+        self.cols_n = cols_n
+        self.block_len = 0.8 * min(height, width)/cols_n
+        self.col_offset = (width - height)/2 + 0.1 * min(height, width) + 0.5 * self.block_len
+        self.row_offset = 0.1 * min(height, width) + 0.5 * self.block_len
+        
+
+    def step(self, reward, obs):
+        """
+        Parameters
+        ----------
+        reward : dict
+            current_score - previous_score
+            
+            key: -1(black), 1(white)
+            value: numbers
+            
+        obs    :  dict 
+            board status
+            key: int 0 ~ 63
+            value: [-1, 0 ,1]
+                    -1 : black
+                     0 : empty
+                     1 : white
+        Returns
+        -------
+        tuple:
+            (x, y) represents position, where (0, 0) mean top left. 
+                x: go right
+                y: go down
+        event_type:
+            non human agent uses pygame.USEREVENT
+        """
+
+        raise NotImplementError("You didn't finish your step function. Please override step function of BaseAgent!")
+        
 class MyAgent(BaseAgent):
     def step(self, obs, reward):
         return (self.col_offset + random.randint(0, self.cols_n-1) * self.block_len, self.row_offset + random.randint(0, self.rows_n-1) * self.block_len), pygame.USEREVENT
